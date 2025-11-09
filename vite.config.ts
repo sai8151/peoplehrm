16ffaa36e93042983db4cba650f3400ab77025ce
B Sai Kiran Reddy <96945198+sai8151@users.noreply.github.com>
2025-11-09 16:54:01 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/peoplehrm/',
+  base: 'https://sai8151.github.io/peoplehrm/',
   server: {
     host: "::",
     port: 8080,
